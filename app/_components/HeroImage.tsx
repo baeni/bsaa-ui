9ee8a3e8ef22014dc4d4b3c76f76bbc71da1a6e2
benny <baeni.saa@gmail.com>
2024-05-29 22:32:19 +0200

    Add HeroImage.tsx component

---
+++ app/_components/HeroImage.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import {gsap} from "gsap";
+import {useEffect} from "react";
+import BackgroundImage from "@/public/bg.jpg";
+
+export default function Test() {
+    useEffect(() => {
+        gsap.to("#headerImageContainer",
+            { padding: "1.25rem", scale: 1, opacity: 1 })
+            .duration(1.5);
+    }, []);
+
+    return (
+        <div id="headerImageContainer" className="w-full h-[100vh] absolute top-0 left-0 z-[-1] scale-110 opacity-0">
+            <img className="w-full h-full object-cover pointer-events-none select-none" src={BackgroundImage.src} alt="Background"/>
+        </div>
+    );
+}
