1ef180d20cf926e01f4190343b031672da51cca8
benny <baeni.saa@gmail.com>
2024-06-03 10:22:53 +0200

    Adjust gsap animations

--- app/_components/HeroSlogan.tsx
+++ app/_components/HeroSlogan.tsx
@@ -1,18 +1,19 @@
 "use client";
 
 import {gsap} from "gsap";
-import {useEffect} from "react";
+import {useGSAP} from "@gsap/react";
 
 export default function HeroSlogan() {
-    useEffect(() => {
-        gsap.to(["#slogan"],
+    useGSAP(() => {
+        gsap.fromTo(["#slogan"],
+            { opacity: "0", scale: "0.95" },
             { opacity: "1", scale: "1" })
             .delay(.5)
             .duration(3);
-    }, []);
+    });
     
     return (
-        <div id="slogan" className="scale-95 opacity-0">
+        <div id="slogan">
             <h1 className="text-6xl md:text-8xl text-white font-bold leading-none pb-8 md:leading-relaxed md:pb-0">Hi, it&apos;s Benny.</h1>
             <p className="text-lg text-neutral-300 leading-relaxed">
                 I try to build wonderful things as a developer with a little big fetish for design.<br/>
