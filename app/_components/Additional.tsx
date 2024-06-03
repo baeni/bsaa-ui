6917e7dab651be1070a86a90b7463c4c5020e392
benny <baeni.saa@gmail.com>
2024-06-03 10:22:28 +0200

    Create temp Additional component

---
+++ app/_components/Additional.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export default function Asdf() {
+    return (
+        <div className="text-white py-[50vh]">
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+            <p>asdf</p>
+        </div>
+    );
+}
