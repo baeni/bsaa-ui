85bdc4cd3e1292ef2fc960dc3dbea1662d901361
benny <baeni.saa@gmail.com>
2024-06-07 13:41:39 +0200

    Visual adjustments to Navbar.tsx

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import {gsap} from "gsap";
 import {useGSAP} from "@gsap/react";
+import Link from "next/link";
 
 export default function Test() {
     useGSAP(() => {
@@ -15,9 +16,9 @@ export default function Test() {
     return (
         <nav id="navbar" className="flex mt-14 w-full fixed top-0 z-50">
             <div className="inline-flex justify-center md:justify-between items-center container">
-                <a className="text-2xl uppercase font-bold text-white" href="/">B.Saa</a>
+                <Link className="text-2xl uppercase font-bold text-white" href="/">B.Saa</Link>
 
-                <a className="hidden md:inline-flex items-center gap-10 px-6 py-3 bg-[#32323225] border border-[#ffffff25] backdrop-blur-sm text-white font-medium rounded-2xl hover:scale-105 transition-transform" href="mailto:baeni.saa@gmail.com">Get in Touch</a>
+                <a className="hidden md:inline-flex items-center gap-10 px-6 py-3 bg-[#ffffff90] backdrop-blur-md text-neutral-700 border border-neutral-200 shadow font-medium rounded-2xl hover:scale-105 transition-transform" href="mailto:baeni.saa@gmail.com">Get in Touch</a>
             </div>
         </nav>
     );
