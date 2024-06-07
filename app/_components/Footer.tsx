c76a5b0617e9915ce2b8aa683d173c2da26199c8
benny <baeni.saa@gmail.com>
2024-06-07 13:03:55 +0200

    Remove GSAP from footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,26 +1,8 @@
 "use client";
 
-import {gsap} from "gsap";
-import{useGSAP} from "@gsap/react";
 import dayjs from "dayjs";
-import {ScrollTrigger} from "gsap/ScrollTrigger";
 
 export default function Test() {
-    useGSAP(() => {
-        gsap.registerPlugin(ScrollTrigger);
-        
-        ScrollTrigger.create({
-            trigger: "footer",
-            start: "10% bottom",
-            onLeave: () => {
-                gsap.to("footer", { opacity: 0, duration: 1 });
-            },
-            onEnterBack: () => {
-                gsap.to("footer", { opacity: 1, duration: 1 });
-            }
-        });
-    });
-
     return (
         <footer id="footer" className="bg-white">
             <div className="container text-sm py-10 grid md:grid-cols-2 justify-between">
