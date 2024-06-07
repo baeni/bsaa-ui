7c43f40dae43596e6574899a6a7009c27d244be2
benny <baeni.saa@gmail.com>
2024-06-07 13:43:50 +0200

    Darken the color of the contact button's text

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -18,7 +18,7 @@ export default function Test() {
             <div className="inline-flex justify-center md:justify-between items-center container">
                 <Link className="text-2xl uppercase font-bold text-white" href="/">B.Saa</Link>
 
-                <a className="hidden md:inline-flex items-center gap-10 px-6 py-3 bg-[#ffffff90] backdrop-blur-md text-neutral-700 border border-neutral-200 shadow font-medium rounded-2xl hover:scale-105 transition-transform" href="mailto:baeni.saa@gmail.com">Get in Touch</a>
+                <a className="hidden md:inline-flex items-center gap-10 px-6 py-3 bg-[#ffffff90] backdrop-blur-md text-neutral-800 border border-neutral-200 shadow font-medium rounded-2xl hover:scale-105 transition-transform" href="mailto:baeni.saa@gmail.com">Get in Touch</a>
             </div>
         </nav>
     );
