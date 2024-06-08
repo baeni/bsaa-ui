512e877c43a4b1d95a05cfc492b7e1fbf84ce209
benny <baeni.saa@gmail.com>
2024-06-08 13:22:44 +0200

    Add wordpress site as hosts for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'bsaa0.wordpress.com'
+            },
+        ],
+    },
+};
 
 export default nextConfig;
