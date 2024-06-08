51183b06ea2bba8059c1b4e930c7308286d310da
benny <baeni.saa@gmail.com>
2024-06-08 17:45:28 +0200

    Remove uppercase class from Navbar.tsx

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -16,7 +16,7 @@ export default function Test() {
     return (
         <nav id="navbar" className="flex mt-14 w-full fixed top-0 z-50">
             <div className="inline-flex justify-center md:justify-between items-center container">
-                <Link className="text-2xl uppercase font-bold text-white" href="/">B.Saa</Link>
+                <Link className="text-2xl font-bold text-white" href="/">.bs</Link>
 
                 <a className="hidden md:inline-flex items-center gap-10 px-6 py-3 bg-[#ffffff90] backdrop-blur-md text-neutral-800 border border-neutral-200 shadow font-medium rounded-2xl hover:scale-105 transition-transform" href="mailto:baeni.saa@gmail.com">Get in Touch</a>
             </div>
