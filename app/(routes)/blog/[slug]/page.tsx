1fefd7b3bb5f9c4ec12f566ed6e0afabe8fe9ec7
benny <baeni.saa@gmail.com>
2024-06-08 12:51:35 +0200

    Add possibility to copy post's link to clipboard

--- app/(routes)/blog/[slug]/page.tsx
+++ app/(routes)/blog/[slug]/page.tsx
@@ -1,25 +1,47 @@
 "use client";
 
+import {useState} from "react";
 import usePostBySlug from "@/app/_hooks/UsePostBySlug";
 import dayjs from "dayjs";
 
 export default function Post({ params }: { params: { slug: string } }) {
     const { post, loading } = usePostBySlug(params.slug);
-    
+    const [copied, setCopied] = useState(false);
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(`https://localhost:3000/blog/${params.slug}`).then(() => {
+            setCopied(true);
+            setTimeout(() => setCopied(false), 5000);
+        });
+    };
     if (loading) {
         return <small className="text-neutral-400">Loading...</small>;
     }
     
     if (!post) {
-        return <small className="text-neutral-400">Loading...</small>;
+        return <small className="text-neutral-400">Error</small>;
     }
 
     return (
         <div className="container mt-40">
             <div className="flex flex-col gap-4 text-black">
                 <h2 className="font-semibold">{post.title}</h2>
-                <p className="text-neutral-600">{post.content.replace(/(<([^>]+)>)/ig, "")}</p>
-                <small className="text-neutral-400">Published on {dayjs(post.date).format("DD.MM.YYYY [at] HH:MM")} by {post.author.name}</small>
+
+                <small className="text-neutral-400">
+                    Published on {dayjs(post.date).format("DD.MM.YYYY [at] HH:MM")} by {post.author.name}
+                </small>
+
+                <div className="text-neutral-600 wp-formatting" dangerouslySetInnerHTML={{__html: post.content}}/>
+
+                <small className="text-neutral-400">
+                    <button className="text-neutral-400 hover:text-black transition-colors" onClick={handleCopy}>
+                        {!copied ? (
+                            <span>Copy link to clipboard</span>
+                        ) : (
+                            <span>Copied to clipboard</span>
+                        )}
+                    </button>
+                </small>
             </div>
         </div>
     );
