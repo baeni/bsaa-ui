00c0e4472b2fac7cf99a93b3c9f7104a4febf6f3
benny <baeni.saa@gmail.com>
2024-06-11 19:47:24 +0200

    Make copying url work dynamically; Visuality adjustments

--- app/(routes)/blog/[slug]/page.tsx
+++ app/(routes)/blog/[slug]/page.tsx
@@ -3,13 +3,16 @@
 import {useState} from "react";
 import usePostBySlug from "@/app/_hooks/UsePostBySlug";
 import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 export default function Post({ params }: { params: { slug: string } }) {
+    dayjs.extend(relativeTime)
+    
     const { post, loading } = usePostBySlug(params.slug);
     const [copied, setCopied] = useState(false);
 
     const handleCopy = () => {
-        navigator.clipboard.writeText(`https://localhost:3000/blog/${params.slug}`).then(() => {
+        navigator.clipboard.writeText(`${window.location.origin}/blog/${params.slug}`).then(() => {
             setCopied(true);
             setTimeout(() => setCopied(false), 5000);
         });
@@ -29,12 +32,14 @@ export default function Post({ params }: { params: { slug: string } }) {
                 <h2 className="font-semibold">{post.title}</h2>
 
                 <small className="text-neutral-400">
-                    Published on {dayjs(post.date_created).format("DD.MM.YYYY [at] HH:MM")} by {post.user_created}
+                    {!post.user_updated
+                        ? `Published by ${post.user_created.first_name} ${dayjs().to(post.date_created)}`
+                        : `Last updated by ${post.user_updated.first_name} ${dayjs().to(post.date_updated)}`}
                 </small>
 
                 <div className="text-neutral-600 wp-formatting">
                     {post.featured_image && (
-                        <img src={post.featured_image} alt="Featured Image"></img>
+                        <img src={`https://cms.imgrio.com/assets/${post.featured_image}`} alt="Featured Image"></img>
                     )}
 
                     <div dangerouslySetInnerHTML={{__html: post.content}}/>
@@ -42,11 +47,10 @@ export default function Post({ params }: { params: { slug: string } }) {
 
                 <small className="text-neutral-400">
                     <button className="text-neutral-400 hover:text-black transition-colors" onClick={handleCopy}>
-                        {!copied ? (
-                            <span>Copy link to clipboard</span>
-                        ) : (
-                            <span>Copied to clipboard</span>
-                        )}
+                        {!copied
+                            ? <span>Copy link to clipboard</span>
+                            : <span>Copied to clipboard</span>
+                        }
                     </button>
                 </small>
             </div>
