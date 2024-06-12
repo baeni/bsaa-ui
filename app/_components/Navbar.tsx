66930b3093d3a8a006e001f45175ef609de493ad
benny <baeni.saa@gmail.com>
2024-06-12 20:44:49 +0200

    Temporarily outcomment Contact button; Replace logo name with Bs.; Visual adjustments

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -14,11 +14,14 @@ export default function Test() {
     });
 
     return (
-        <nav id="navbar" className="flex mt-14 w-full fixed top-0 z-50">
+        <nav id="navbar" className="flex mt-14 w-full fixed top-0 z-50 mix-blend-difference">
             <div className="inline-flex justify-center md:justify-between items-center container">
-                <Link className="text-2xl font-bold text-white" href="/">.bs</Link>
+                <Link className="text-black font-bold text-3xl -tracking-[5px]" href="/">
+                    Bs.
+                </Link>
 
-                <a className="hidden md:inline-flex items-center gap-10 px-6 py-3 bg-[#ffffff90] backdrop-blur-md text-neutral-800 border border-neutral-200 shadow font-medium rounded-2xl hover:scale-105 transition-transform" href="mailto:baeni.saa@gmail.com">Get in Touch</a>
+                {/*<a className="hidden md:inline-flex items-center gap-10 px-4 py-2 bg-black text-white font-medium"*/}
+                {/*   href="mailto:baeni.saa@gmail.com">Get in Touch</a>*/}
             </div>
         </nav>
     );
