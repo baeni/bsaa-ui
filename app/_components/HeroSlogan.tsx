79ebcec6e3c4f6f1b9e373e4d81eabd6a9ce47f0
benny <baeni.saa@gmail.com>
2024-06-12 21:27:51 +0200

    Decrease opacity and add blur on scroll progress

--- app/_components/HeroSlogan.tsx
+++ app/_components/HeroSlogan.tsx
@@ -2,15 +2,22 @@
 
 import {gsap} from "gsap";
 import {useGSAP} from "@gsap/react";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
 import Link from "next/link";
 
 export default function HeroSlogan() {
     useGSAP(() => {
         gsap.fromTo(["#slogan"],
-            { opacity: 0, scale: 0.95 },
-            { opacity: 1, scale: 1 })
+            { opacity: 0, scale: 0.95, filter: "blur(2px)" },
+            { opacity: 1, scale: 1, filter: "blur(0px)" })
             .delay(0.5)
             .duration(3);
+
+        ScrollTrigger.create({
+            onUpdate: self => {
+                gsap.to("#slogan", { opacity: 1 - self.progress, scale: 1 + 0.1 * self.progress, filter: `blur(${10 * self.progress}px)`, duration: 0.5 });
+            }
+        });
     });
     
     return (
