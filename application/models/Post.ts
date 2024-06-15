f0853f2765a0085579a9cd4cee333a0f6e3107b1
benny <baeni.saa@gmail.com>
2024-06-15 16:57:17 +0200

    Remove quotes from property identifiers

--- application/models/Post.ts
+++ application/models/Post.ts
@@ -1,18 +1,18 @@
 import { User } from "@/application/models/User";
 
 export interface Post {
-    "id": string,
-    "status": PostStatus,
-    "user_created": User,
-    "date_created": string,
-    "user_updated": User | null,
-    "date_updated": string | null,
-    "title": string,
-    "content": string,
-    "excerpt": string,
-    "thumbnail": string | null,
-    "featured_image": string | null,
-    "slug": string
+    id: string,
+    status: PostStatus,
+    user_created: User,
+    date_created: string,
+    user_updated: User | null,
+    date_updated: string | null,
+    title: string,
+    content: string,
+    excerpt: string,
+    thumbnail: string | null,
+    featured_image: string | null,
+    slug: string
 }
 
 export enum PostStatus {
