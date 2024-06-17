311d7775ca87ac51c2a2305c982fabf9a2b04719
benny <baeni.saa@gmail.com>
2024-06-17 18:05:12 +0200

    Remove container class from seciont and min-h-screen class from outter div

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -14,8 +14,8 @@ export default function NotFound() {
     });
     
     return (
-        <div id="notFound" className="min-h-screen">
-            <section className="container h-screen flex flex-col items-center justify-center text-center gap-6">
+        <div id="notFound">
+            <section className="h-screen flex flex-col items-center justify-center text-center gap-6">
                 <h1 className="text-6xl md:text-9xl bg-clip-text text-transparent font-black bg-[url('/bg.jpg')] bg-bottom">Oops!</h1>
                 <h2 className="text-xl font-bold uppercase">404 – Page not found</h2>
                 <p className="text-lg text-neutral-400 leading-relaxed">The page your are looking for might have been
