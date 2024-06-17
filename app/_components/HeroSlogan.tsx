8f0f97870eb66b98e25b63a61b614c472354063a
benny <baeni.saa@gmail.com>
2024-06-17 13:47:17 +0200

    Add slight rotation effect for slogan on load and scroll

--- app/_components/HeroSlogan.tsx
+++ app/_components/HeroSlogan.tsx
@@ -8,8 +8,8 @@ import Link from "next/link";
 export default function HeroSlogan() {
     useGSAP(() => {
         gsap.fromTo("#slogan",
-            { opacity: 0, scale: 0.95, filter: "blur(5px)" },
-            { opacity: 1, scale: 1, filter: "blur(0px)" })
+            { opacity: 0, scale: 0.95, filter: "blur(5px)", rotate: 2 },
+            { opacity: 1, scale: 1, filter: "blur(0px)", rotate: 0 })
             .delay(0.5)
             .duration(3);
 
@@ -17,13 +17,13 @@ export default function HeroSlogan() {
             trigger: "#slogan",
             start: "center+=0.51 center",
             onUpdate: self => {
-                gsap.to("#slogan", { opacity: 1 - self.progress, filter: `blur(${10 * self.progress}px)` });
+                gsap.to("#slogan", { opacity: 1 - self.progress, filter: `blur(${10 * self.progress}px)`, rotate: -2.5 * self.progress });
             }
         });
     });
     
     return (
-        <div id="slogan" className="w-full">
+        <div id="slogan" className="w-full origin-left">
             <h1 className="text-6xl md:text-8xl text-white font-bold leading-none pb-8 md:leading-relaxed md:pb-0">Hi, it&apos;s Benny.</h1>
             <p className="text-lg text-neutral-300 leading-relaxed">
                 I try to build wonderful things as a developer with a little big fetish for design.<br/>
