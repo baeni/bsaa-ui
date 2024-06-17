7ccc0e9aaaa227129bbe96b052b70f15ae7e9f98
benny <baeni.saa@gmail.com>
2024-06-17 17:56:57 +0200

    Replace Blog component with a div containing PostLists component; Remove container class as this is implemented in layout.tsx parent

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,18 +1,24 @@
-import HeroImage from "../_components/HeroImage";
-import HeroSlogan from "../_components/HeroSlogan";
-import Blog from "../_components/Blog";
+import HeroImage from "../_components/Hero/HeroImage";
+import HeroSlogan from "../_components/Hero/HeroSlogan";
+import React from "react";
+import PostList from "@/app/_components/Blog/PostList";
+import Link from "next/link";
 
 export default function Home() {
     return (
         <div className="min-h-screen">
             {/* Hero */}
-            <section className="container h-screen flex items-center">
+            <section className="h-screen flex items-center">
                 <HeroImage />
                 <HeroSlogan />
             </section>
-            
-            <section className="container mt-[25vh] py-20">
-                <Blog />
+
+            {/* Blog Preview */}
+            <section className="mt-[25vh] py-20">
+                <div id="blog" className="scroll-mt-10 flex flex-col gap-10">
+                    <Link className="text-neutral-400 font-medium" href="/blog">Blog</Link>
+                    <PostList limit={2} />
+                </div>
             </section>
         </div>
     );
