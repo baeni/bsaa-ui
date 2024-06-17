9473222b94413c9995bf7da98a2631799ffb908d
benny <baeni.saa@gmail.com>
2024-06-17 17:02:33 +0200

    Add fromTo animation for #notFound

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,20 @@
+"use client";
+
+import {gsap} from "gsap";
+import {useGSAP} from "@gsap/react";
 import Link from "next/link";
 
 export default function NotFound() {
+    useGSAP(() => {
+        gsap.fromTo("#notFound",
+            { opacity: 0, scale: 0.95, filter: "blur(5px)", rotate: 2 },
+            { opacity: 1, scale: 1, filter: "blur(0px)", rotate: 0 })
+            .delay(0.5)
+            .duration(3);
+    });
+    
     return (
-        <div className="min-h-screen">
+        <div id="notFound" className="min-h-screen">
             <section className="container h-screen flex flex-col items-center justify-center text-center gap-6">
                 <h1 className="text-6xl md:text-9xl bg-clip-text text-transparent font-black bg-[url('/bg.jpg')] bg-bottom">Oops!</h1>
                 <h2 className="text-xl font-bold uppercase">404 – Page not found</h2>
