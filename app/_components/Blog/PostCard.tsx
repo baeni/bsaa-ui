06ea9d6f524fa3a8e4fa9a32fa5c6afcdee8dd88
benny <baeni.saa@gmail.com>
2024-06-17 18:31:56 +0200

    Show estimated reading time

--- app/_components/Blog/PostCard.tsx
+++ app/_components/Blog/PostCard.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import {Post} from "@/application/models/Post";
 import Link from "next/link";
+import ReadingTime from "reading-time";
 
 export default function PostCard(props: Props) {
     return (
         <Link className="group flex flex-col gap-4 text-black" href={`/blog/${props.post.slug}`}>
-            <div className="grid grid-cols-[auto_minmax(1.5rem,1fr)] gap-3 items-center">
-                <h2 className="font-semibold truncate" title={props.post.title}>{props.post.title}</h2>
-                <svg
-                    className="stroke-neutral-400 size-5 origin-left group-hover:scale-x-125 transition-transform"
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                    stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round"
-                          d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"/>
-                </svg>
+            <div>
+                <small className="text-neutral-400">
+                    {ReadingTime(props.post.content).text}
+                </small>
+                <div className="grid grid-cols-[auto_minmax(1.5rem,1fr)] gap-3 items-center">
+                    <h2 className="font-semibold truncate" title={props.post.title}>{props.post.title}</h2>
+                    <svg
+                        className="stroke-neutral-400 size-5 origin-left group-hover:scale-x-125 transition-transform"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                        stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round"
+                              d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"/>
+                    </svg>
+                </div>
             </div>
             {props.post.featured_image && (
                 <img className="aspect-[16/10] object-cover"
