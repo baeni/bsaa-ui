534cd17564a824fcd9eaca4b56834895fa0ddaa3
benny <baeni.saa@gmail.com>
2024-06-18 10:50:02 +0200

    Remove container class

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <footer id="footer" className="bg-white">
-            <div className="container text-sm py-10 grid md:grid-cols-2 justify-between">
+            <div className="text-sm py-10 grid md:grid-cols-2 justify-between">
                 <ul className="inline-flex gap-4">
                     <li><a className="text-neutral-400 font-medium underline decoration-dotted"
                            href="mailto:baeni.saa@gmail.com">Contact</a></li>
