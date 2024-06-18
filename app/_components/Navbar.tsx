9b761e6ea1aca0a5081352addd7940ade67c9993
benny <baeni.saa@gmail.com>
2024-06-18 10:50:47 +0200

    Remove unnecessary div from Navbar.tsx

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -14,12 +14,10 @@ export default function Navbar() {
     });
 
     return (
-        <nav id="navbar" className="flex mt-14 w-full fixed top-0 z-50">
-            <div className="inline-flex justify-center md:justify-between items-center container">
-                <Link className="text-black font-bold text-3xl -tracking-[5px]" href="/">
-                    Bs.
-                </Link>
-            </div>
+        <nav id="navbar" className="flex justify-between mt-14 w-full fixed top-0 z-50">
+            <Link className="font-bold text-3xl -tracking-[5px]" href="/">
+                Bs.
+            </Link>
         </nav>
     );
 }
