e517310392c448e923e5ec20711be74456bfe300
benny <baeni.saa@gmail.com>
2024-06-19 21:02:19 +0200

    Make use of constants.ts; Replace id with useRef

--- app/_components/Hero/HeroSlogan.tsx
+++ app/_components/Hero/HeroSlogan.tsx
@@ -3,27 +3,35 @@
 import {gsap} from "gsap";
 import {useGSAP} from "@gsap/react";
 import {ScrollTrigger} from "gsap/ScrollTrigger";
+import * as Constants from "@/app/constants";
+import {useRef} from "react";
 
 export default function HeroSlogan() {
+    const sloganRef = useRef<HTMLDivElement>(null);
+    
     useGSAP(() => {
-        gsap.fromTo("#slogan",
-            { opacity: 0, scale: 0.95, filter: "blur(5px)", rotate: 2 },
-            { opacity: 1, scale: 1, filter: "blur(0px)", rotate: 0, delay: 0.5, duration: 3 });
+        const slogan = sloganRef.current;
+        
+        if (slogan) {
+            gsap.fromTo(slogan,
+                { opacity: 0, scale: 0.95, filter: "blur(5px)", rotate: 2 },
+                { opacity: 1, scale: 1, filter: "blur(0px)", rotate: 0, delay: 0.5, duration: 3 });
 
-        ScrollTrigger.create({
-            trigger: "#slogan",
-            start: "center+=0.51 center",
-            onUpdate: self => {
-                gsap.to("#slogan", { opacity: 1 - self.progress, filter: `blur(${10 * self.progress}px)`, rotate: -2.5 * self.progress });
-            }
-        });
+            ScrollTrigger.create({
+                trigger: slogan,
+                start: "center+=0.51 center",
+                onUpdate: self => {
+                    gsap.to(slogan, { opacity: 1 - self.progress, filter: `blur(${10 * self.progress}px)`, rotate: -2.5 * self.progress });
+                }
+            });
+        }
     });
     
     return (
-        <div id="slogan" className="flex flex-col gap-4 md:gap-6 w-full origin-left mix-blend-color-dodge">
+        <div ref={sloganRef} className="flex flex-col gap-4 md:gap-6 w-full origin-left mix-blend-color-dodge">
             <h1 className="text-5xl md:text-7xl text-neutral-300 font-bold leading-none uppercase">
                 Hello,<br/>
-                it&apos;s Benny.
+                it&apos;s {Constants.NICKNAME}.
             </h1>
             <p className="text-md md:text-xl text-neutral-400 leading-snug text-wrap">
                 I try to build wonderful things<br/>
