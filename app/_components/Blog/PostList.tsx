41390589d391945963fc69c35f73d433dd365068
benny <baeni.saa@gmail.com>
2024-06-19 16:37:00 +0200

    Add callback to notify when loaded content

--- app/_components/Blog/PostList.tsx
+++ app/_components/Blog/PostList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, {useEffect} from "react";
 import usePosts, {SortOrder} from "@/app/_hooks/UsePosts";
 import {Post} from "@/application/models/Post";
 import Link from "next/link";
@@ -8,6 +8,12 @@ import PostCard from "@/app/_components/Blog/PostCard";
 
 export default function PostsList(props: Props) {
     const { posts, loading } = usePosts(props.limit, "date_created", SortOrder.Descending);
+
+    useEffect(() => {
+        if (!loading) {
+            props.onLoaded && props.onLoaded();
+        }
+    }, [loading, props.onLoaded]);
     
     return (
         <div className="grid md:grid-cols-2 gap-12">
@@ -23,5 +29,6 @@ export default function PostsList(props: Props) {
 }
 
 interface Props {
-    limit?: number
+    limit?: number,
+    onLoaded?: () => void
 }
