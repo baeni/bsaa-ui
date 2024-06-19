e0a3e945dcf4355327d880c82980c88619d02658
benny <baeni.saa@gmail.com>
2024-06-19 16:36:21 +0200

    Minor adjustments to gsap

--- app/_components/Hero/HeroSlogan.tsx
+++ app/_components/Hero/HeroSlogan.tsx
@@ -3,15 +3,12 @@
 import {gsap} from "gsap";
 import {useGSAP} from "@gsap/react";
 import {ScrollTrigger} from "gsap/ScrollTrigger";
-import Link from "next/link";
 
 export default function HeroSlogan() {
     useGSAP(() => {
         gsap.fromTo("#slogan",
             { opacity: 0, scale: 0.95, filter: "blur(5px)", rotate: 2 },
-            { opacity: 1, scale: 1, filter: "blur(0px)", rotate: 0 })
-            .delay(0.5)
-            .duration(3);
+            { opacity: 1, scale: 1, filter: "blur(0px)", rotate: 0, delay: 0.5, duration: 3 });
 
         ScrollTrigger.create({
             trigger: "#slogan",
