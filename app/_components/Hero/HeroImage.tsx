a0ff86b0ca226b3a6d279e6ecf1167d99c8671db
benny <baeni.saa@gmail.com>
2024-06-19 16:34:37 +0200

    Replace id reference with useRef

--- app/_components/Hero/HeroImage.tsx
+++ app/_components/Hero/HeroImage.tsx
@@ -1,39 +1,45 @@
 "use client";
 
+import {useRef} from "react";
 import {gsap} from "gsap";
 import {useGSAP} from "@gsap/react";
 import {ScrollTrigger} from "gsap/ScrollTrigger";
 import BackgroundImage from "@/public/bg.jpg";
-import Image from "next/image";
 
 export default function HeroImage() {
+    const imageWrapperRef = useRef<HTMLDivElement>(null);
+    const imageRef = useRef<HTMLImageElement>(null);
+    
     useGSAP(() => {
+        const imageWrapper = imageWrapperRef.current;
+        const image = imageRef.current;
+        
         gsap.registerPlugin(ScrollTrigger);
         
-        gsap.from("#heroImageContainer",
-            { scale: 1.1, opacity: 0, padding: 0 });
+        if (imageWrapper && image) {
+            gsap.fromTo(imageWrapper,
+                { scale: 1.1, opacity: 0, padding: 0 },
+                { scale: 1, opacity: 1, duration: 1.5 }
+            );
 
-        gsap.to("#heroImageContainer",
-            { scale: 1, opacity: 1 })
-            .duration(1.5);
-
-        ScrollTrigger.create({
-            trigger: "#heroImageContainer",
-            start: "top top",
-            end: "+=25%",
-            pin: true,
-            onLeave: () => {
-                gsap.to("#heroImage", { padding: "0.75rem", duration: 0.175 });
-            },
-            onEnterBack: () => {
-                gsap.to("#heroImage", { padding: "0rem", duration: 0.175 });
-            }
-        });
+            ScrollTrigger.create({
+                trigger: imageWrapper,
+                start: "top top",
+                end: "+=25%",
+                pin: true,
+                onLeave: () => {
+                    gsap.to(image, { padding: "0.75rem", duration: 0.175 });
+                },
+                onEnterBack: () => {
+                    gsap.to(image, { padding: "0rem", duration: 0.175 });
+                }
+            });
+        }
     })
 
     return (
-        <div id="heroImageContainer" className="w-screen h-screen absolute top-0 left-0 z-[-1]">
-            <img id="heroImage" className="w-full h-full object-cover pointer-events-none select-none"
+        <div ref={imageWrapperRef} className="w-screen h-screen absolute top-0 left-0 z-[-1]">
+            <img ref={imageRef} className="w-full h-full object-cover pointer-events-none select-none"
                  src={BackgroundImage.src} alt="Background"
             />
         </div>
