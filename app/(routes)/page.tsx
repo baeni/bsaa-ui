825e0f6f3ea93fa4955df6bc0873f4590afdb9b3
benny <baeni.saa@gmail.com>
2024-06-19 16:36:43 +0200

    Render Contact section only when PostList.tsx loaded its content

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,11 +1,19 @@
+"use client";
+
 import HeroImage from "../_components/Hero/HeroImage";
 import HeroSlogan from "../_components/Hero/HeroSlogan";
-import React from "react";
+import React, {useState} from "react";
 import PostList from "@/app/_components/Blog/PostList";
 import Link from "next/link";
-import ContactForm from "@/app/_components/ContactForm";
+import Contact from "@/app/_components/Contact";
 
 export default function Home() {
+    const [isPostListLoaded, setIsPostListLoaded] = useState(false);
+
+    const handlePostListLoaded = () => {
+        setIsPostListLoaded(true);
+    };
+    
     return (
         <div className="min-h-screen">
             {/* Hero */}
@@ -18,14 +26,16 @@ export default function Home() {
             <section className="mt-[25vh] py-20">
                 <div id="blog" className="scroll-mt-10 flex flex-col gap-10">
                     <Link className="text-neutral-400 font-medium" href="/blog">Blog</Link>
-                    <PostList />
+                    <PostList onLoaded={handlePostListLoaded} />
                 </div>
             </section>
             
             {/* Contact */}
-            <section className="pb-20">
-                <ContactForm />
-            </section>
+            {isPostListLoaded && (
+                <section className="pb-20">
+                    <Contact />
+                </section>
+            )}
         </div>
     );
 }
