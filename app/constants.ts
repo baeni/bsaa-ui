5ace0b146179d465f13bae048787abfe08a0f77c
benny <baeni.saa@gmail.com>
2024-06-25 22:32:23 +0200

    Add constant FADE_BLUR_INIT_VAL

--- app/constants.ts
+++ app/constants.ts
@@ -1,3 +1,5 @@
+// literals
+// general
 export const PAGE_TITLE = "bsaa"
 export const NICKNAME = "Benny"
 export const API_BASE_URL = "https://cms.imgrio.com"
@@ -9,3 +11,9 @@ export const ADDRESS = "Charlotte-Temming-Str. 3"
 export const CITY = "44135 Dortmund"
 export const PHONE_NUMBER = "auf Anfrage"
 export const EMAIL_ADDRESS = "baeni.saa@gmail.com"
+
+// numbers
+// general
+export const FADE_BLUR_INIT_VAL = 10
+
+
