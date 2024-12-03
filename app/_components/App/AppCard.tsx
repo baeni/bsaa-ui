76c082e3fd01d943b6db037db7b9cb65936edb9f
benny <baeni.saa@gmail.com>
2024-12-03 16:38:34 +0100

    Delete out-commented code

--- app/_components/App/AppCard.tsx
+++ app/_components/App/AppCard.tsx
@@ -45,30 +45,6 @@ export default function AppCard(props: Props) {
                          alt="Thumbnail"/>
                 )}
             </div>
-            {/*<div>*/}
-            {/*    <small className="text-neutral-400">*/}
-            {/*        {ReadingTime(props.post.content).text}*/}
-            {/*    </small>*/}
-            {/*    <div className="grid grid-cols-[auto_minmax(1.5rem,1fr)] gap-3 items-center">*/}
-            {/*        <h2 className="text-lg md:text-base font-semibold truncate" title={props.post.title}>{props.post.title}</h2>*/}
-            {/*        <svg*/}
-            {/*            className="stroke-neutral-400 size-5 origin-left group-hover:scale-x-125 transition-transform"*/}
-            {/*            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}*/}
-            {/*            stroke="currentColor">*/}
-            {/*            <path strokeLinecap="round" strokeLinejoin="round"*/}
-            {/*                  d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"/>*/}
-            {/*        </svg>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-            {/*{props.post.featured_image && (*/}
-            {/*    <img className="aspect-[16/10] object-cover"*/}
-            {/*         src={`https://cms.imgrio.com/assets/${props.post.featured_image}?quality=20`}*/}
-            {/*         alt="Thumbnail"/>*/}
-            {/*)}*/}
-            {/*<p className="text-sm text-neutral-600 whitespace-pre-line">{props.post.excerpt}</p>*/}
-            {/*<small className="text-neutral-400">*/}
-            {/*    Published by {props.post.user_created.first_name}*/}
-            {/*</small>*/}
         </Link>
     );
 }
